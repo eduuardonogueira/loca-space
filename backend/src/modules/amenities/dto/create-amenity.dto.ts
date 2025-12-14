a7a27c172cf526747447a2b6490cff69390b5233
Paulo Moraes <paulompj010@gmail.com>
2025-12-14 20:01:20 -0300

    FIX: Atualizando Modulos

--- backend/src/modules/amenities/dto/create-amenity.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class CreateAmenityDto {
-  @IsNotEmpty()
-  name: string;
-}
--- backend/src/modules/amenities/dto/update-amenity.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAmenityDto } from './create-amenity.dto';
-import { IsNotEmpty } from 'class-validator';
-
-export class UpdateAmenityDto extends PartialType(CreateAmenityDto) {
-  @IsNotEmpty()
-  name: string;
-}
