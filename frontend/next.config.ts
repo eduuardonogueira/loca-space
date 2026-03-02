da7fbbd9cbe1203a1df9f4db0e1daf83b884eed5
Paulo Moraes <paulo.moraes@fisetech.com>
2026-03-02 15:16:43 -0300

    Fix: Remove config next

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,9 +3,6 @@ import { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  env: {
-    BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || process.env.BACKEND_URL || "",
-  },
   images: {
     remotePatterns: [
       {
